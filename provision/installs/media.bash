8579f335591f055a3a2fc67b4d86e140664911b3
Software Shinobi <the.software.shinobi@gmail.com>
2024-08-18 17:25:08 -0500

    automated terminal push

--- provision/installs/media.bash
+++ provision/installs/media.bash
@@ -2,5 +2,5 @@
 outputDIR=/tmp/media/backgrounds/
 outputCNT=4
 
-cp /provision/media/Backgrounds/desktop.png /usr/share/lxqt/themes/light/simple_blue_widescreen.png
+cp provision/media/Backgrounds/desktop.png /usr/share/lxqt/themes/light/simple_blue_widescreen.png
 
